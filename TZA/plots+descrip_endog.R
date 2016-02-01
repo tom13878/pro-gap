24f1ba0f55690419e1926161e45badce11799ce5
tom13878 <tomas.morley@wur.nl>
2016-02-01 10:10:43 +0100

    Added new plots and tables for describing the data

--- TZA/plots+descrip_endog.R
+++ TZA/plots+descrip_endog.R
@@ -4,34 +4,85 @@
 
 # --------------------------------------
 # read in the data
+
 setwd("c:/users/tomas/documents/lei")
 endog <- readRDS("endog.rds")
 
-# table by region
-tab1 <- xtabs(vouchTotal ~ factor(region_name_lsms) + factor(surveyyear), data=endog)
-tab2 <- xtabs(vouchAny ~ factor(region_name_lsms) + factor(surveyyear), data=endog)
-tab3 <- table(region=factor(endog$region_name_lsms), year=factor(endog$surveyyear))
-
-year_marg <- apply(tab3, 1, sum)
-reg_marg <- apply(tab3, 2, sum)
-
-round(tab1/year_marg, 2)
-round(tab1/reg_marg, 2)
-
-# table by zone
-
-tab1 <- xtabs(vouchTotal ~ factor(zone) + factor(surveyyear), data=endog)
-tab2 <- xtabs(vouchAny ~ factor(zone) + factor(surveyyear), data=endog)
-tab3 <- table(region=factor(endog$zone), year=factor(endog$surveyyear))
-
-year_marg <- apply(tab3, 1, sum)
-zone_marg <- apply(tab3, 2, sum)
-
-round(tab1/year_marg, 2)
-round(tab1/zone_marg, 2)
+# make factor variables
+endog$region_name_lsms <- factor(endog$region_name_lsms)
+endog$surveyyear <- factor(endog$surveyyear)
 
 # -------------------------------------
+# start by trying to find out how many
+# farmers received a voucher by region
+# each year
 
-plot(asset ~ factor(vouchAny), data=endog[endog$asset < median(endog$asset), ])
-plot(vouchTotal ~ factor(sex), data=endog[endog$vouchTotal > 0,])
-plot(vouchTotal ~ factor(zone), data=endog)
+tab.households <- table(region=endog$region_name_lsms[drop=T], year=endog$surveyyear[drop=T])
+tab.vouchAny <- xtabs(vouchAny ~ region_name_lsms + surveyyear, data=endog)
+
+# this table shows the percentage of households
+# who did receive a voucher by region and year
+
+round(tab.vouchAny/tab.households*100, 2)
+
+# -------------------------------------
+# find out total number of vouchers 
+# received each year and region
+
+tab.vouchTotal <- xtabs(vouchTotal ~ region_name_lsms + surveyyear, data=endog)
+
+# tables to show who those vouchers went to
+# each year and region
+
+tab.year.pc <- round(prop.table(tab.vouchTotal, 1)*100, 2)
+tab.reg.pc <- round(prop.table(tab.vouchTotal, 2)*100, 2)
+
+# -------------------------------------
+# repeat above tables for zones
+
+
+# -------------------------------------
+# plots
+# 1. boxplots of those who received a 
+# voucher and those who did not
+# 2. check for normality of linear
+# predictors
+
+library(ggplot2)
+
+# boxplots
+
+ggplot(endog, aes(x=factor(vouchAny), y=log(asset))) +
+  geom_boxplot() +
+  facet_grid(~surveyyear)
+ 
+ggplot(endog, aes(x=factor(sex), y=vouchTotal)) +
+  geom_boxplot() +
+  facet_grid(~surveyyear) 
+
+ggplot(endog, aes(x=factor(zone), y=vouchTotal)) +
+  geom_boxplot() +
+  facet_wrap(~surveyyear) 
+
+# need to facet these plots
+plot(dist2town ~ factor(vouchAny), data=endog)
+plot(dist2market ~ factor(vouchAny), data=endog)
+
+# normality check
+
+library(ggplot2)
+
+ggplot(endog) +
+  geom_histogram(aes(log(asset), ..density..)) +
+  facet_grid(~ surveyyear, scales = "free") +
+  geom_density(aes(log(asset), ..density..))
+
+ggplot(endog) +
+  facet_grid(~ surveyyear, scales = "free") +
+  geom_density(aes(log(asset), ..density..))
+
+# try some component plus residual plots using the
+# predictors for the first stage of the analysis
+# to see what the distribution of the error term
+# is, if it is normal -> probit, otherwise might
+# be logit distributed.
