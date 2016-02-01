d2faa278b2b8097d8934adf949e75c6affa56782
tom13878 <tomas.morley@wur.nl>
2016-02-01 10:11:26 +0100

    Added some goodness of fit checks for first stage model

--- TZA/endog_analysis.R
+++ TZA/endog_analysis.R
@@ -5,28 +5,56 @@
 options(scipen=999)
 
 # -------------------------------------
-# read in the prepared data
+# read in the prepared data and select
+# only complete rows
+
 setwd("c:/users/tomas/documents/lei")
 endog <- readRDS("endog.rds")
+endog <- endog[complete.cases(endog),]
 
 # -------------------------------------
-# first stage model
+# first stage model - logit/probit model
+# (1/0) if farmer received a voucher or not
 
-modl <- glm(cbind(vouchAny, 1-vouchAny) ~ surveyyear + ccm_prez10*split_prez10 +
-              offset(log(vtot)) + log(households) + dist2HQ + dist2market + dist2town +
-              years + log(asset) + age + educ1555,
+modl.logit <- glm(cbind(vouchAny, 1-vouchAny) ~ surveyyear + ccm_prez10*split_prez10 +
+              log(vtot) + log(households) + dist2HQ + dist2market + dist2town +
+              years + log(asset) + age + education + education1555 + N1555,
             family=binomial, data=endog)
-r1 <- residuals(modl)
+
+modl.probit <- glm(cbind(vouchAny, 1-vouchAny) ~ surveyyear + ccm_prez10*split_prez10 +
+                     log(vtot) + log(households) + dist2HQ + dist2market + dist2town +
+                     years + log(asset) + age + education + education1555 + N1555,
+                   family=binomial(link=probit), data=endog)
+
+# test for goodness of fit on the logit model
+predicted <- as.numeric(
+  predict.glm(modl.logit, type="response")>.5)
+true <- endog$vouchAny
+correct <- as.numeric(predicted == true)
+100*table(correct)/sum(table(correct)) # model is correct 87% of the time
+
+# test for goodness of fit on the probit model
+predicted <- as.numeric(
+  predict.glm(modl.probit, type="response")>.5)
+true <- endog$vouchAny
+correct <- as.numeric(predicted == true)
+100*table(correct)/sum(table(correct))
+
+# extract residuals from probit
+r1 <- residuals(modl.probit, type="pearson")
 
 # -------------------------------------
 # second stage model - HH chars and plot chars
 library(AER)
 
-modl2 <- tobit(N ~ dist2HQ + dist2market + dist2town + sex + age + years + rural +
-                 educ + educ1555 + log(asset) + zone + surveyyear + legume + hh_slope + r1
-               , data=endog2)
+# include soil variables. And other variables leading to higher demand for fertilizer.
 
-r2 <- residuals(modl2)
+modl2 <- tobit(N ~ dist2HQ + dist2market + dist2town + sex + age + years + rural +
+                 education + education1555 + N1555 + log(asset) + zone + surveyyear +
+                 legume + hh_slope + r1
+               , data=endog)
+
+r2 <- residuals(modl2, type="pearson")
 
 # bootstrap se
 
@@ -40,18 +68,19 @@ stargazer(modl, type = "text",
 
 # -------------------------------------
 # third stage model - example only
-# get real version form Michiel
+# get real version from Michiel
 
-modl3 <- lm(yld ~ N + dist2HQ + dist2market + dist2town + sex + age + years + rural +
-              educ + educ1555 + log(asset) + zone + surveyyear + legume + hh_slope + r1,
-            data=endog2)
+modl3 <- lm(yld ~ N + I(N^2) + dist2HQ + dist2market + dist2town + sex + age + years + rural +
+              education + education1555 + log(asset) + zone + surveyyear + legume +
+              hh_slope + r2,
+            data=endog)
 
 # bootstrap se
-
+library(stargazer)
 coeftest(modl3, df = Inf, vcov = vcovHC(modl3, type = "HC1"))
 
 cov1        <- vcovHC(modl3, type = "HC1")
 robust.se1   <- sqrt(diag(cov1))
 
-stargazer(modl, type = "text",
+stargazer(modl3, type = "text",
           se = list(NULL, robust.se1), intercept.bottom = FALSE, digits = 2, digits.extra = 2)
