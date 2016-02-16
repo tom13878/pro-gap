53fb42b9c00d9c847621e7cb77930b5b761f5ce1
tom13878 <tomas.morley@wur.nl>
2016-02-16 19:10:38 +0100

    bootstrapped se and added probit model goodness of fit test

--- TZA/endog_analysis.R
+++ TZA/endog_analysis.R
@@ -16,71 +16,94 @@ endog <- endog[complete.cases(endog),]
 # first stage model - logit/probit model
 # (1/0) if farmer received a voucher or not
 
-modl.logit <- glm(cbind(vouchAny, 1-vouchAny) ~ surveyyear + ccm_prez10*split_prez10 +
-              log(vtot) + log(households) + dist2HQ + dist2market + dist2town +
-              years + log(asset) + age + education + education1555 + N1555,
-            family=binomial, data=endog)
+# time averages? It would just be the assets,
+# everything else is likely to be invariable 
+# over time
 
 modl.probit <- glm(cbind(vouchAny, 1-vouchAny) ~ surveyyear + ccm_prez10*split_prez10 +
-                     log(vtot) + log(households) + dist2HQ + dist2market + dist2town +
+                     log(vtot) + dist2HQ + dist2market + dist2town +
                      years + log(asset) + age + education + education1555 + N1555,
                    family=binomial(link=probit), data=endog)
 
-# test for goodness of fit on the logit model
-predicted <- as.numeric(
-  predict.glm(modl.logit, type="response")>.5)
-true <- endog$vouchAny
-correct <- as.numeric(predicted == true)
-100*table(correct)/sum(table(correct)) # model is correct 87% of the time
+# goodness of fit test for the probit 
+# model - percent correctly predicted
 
-# test for goodness of fit on the probit model
 predicted <- as.numeric(
   predict.glm(modl.probit, type="response")>.5)
 true <- endog$vouchAny
 correct <- as.numeric(predicted == true)
 100*table(correct)/sum(table(correct))
 
+# also do percentage correctly predicted for
+# each outcome (y=1 and y=0)
+
+correct1 <- as.numeric(predicted[true == 1] == true[true == 1])
+100*table(correct1)/sum(table(correct1)) # correct only 23.5 % of the time
+correct0 <- as.numeric(predicted[true == 0] == true[true == 0])
+100*table(correct0)/sum(table(correct0)) # correct 97% of the time
+
+# overall percent correctly predicted as above.
+# Show that it is the same as weighted average
+# problem here is that the model is good at 
+# predicting 0s and not 1s
+
+prop1.true <- mean(true)
+prop0.pred <- mean(correct0)
+prop1.pred <- mean(correct1)
+(1-prop1.true)*prop0.pred + prop1.true*prop1.pred # 0.87
+
 # extract residuals from probit
+# and move on with analysis anyway
+# here I use pearson residuals. Altrnatives
+# are deviance residuals, working residuals
+# or response residuals. Best is probably the
+# pearson residuals
+
 r1 <- residuals(modl.probit, type="pearson")
 
 # -------------------------------------
-# second stage model - HH chars and plot chars
-library(AER)
+# second stage model - tobit model for adoption
+# of nitrogen using residuals from the first stage
+# and HH chars and plot chars as covariates
+
+library(AER) # for tobit function
 
 # include soil variables. And other variables leading to higher demand for fertilizer.
 
-modl2 <- tobit(N ~ dist2HQ + dist2market + dist2town + sex + age + years + rural +
+modl.tobit <- tobit(N ~ dist2HQ + dist2market + dist2town + sex + age + years + rural +
                  education + education1555 + N1555 + log(asset) + zone + surveyyear +
                  legume + hh_slope + r1
                , data=endog)
 
-r2 <- residuals(modl2, type="pearson")
+# bootstrap se from the AER book ->
+# recall that tobit is estimated via
+# ml, so z-score not t-score
 
-# bootstrap se
-
-coeftest(modl2, df = Inf, vcov = vcovHC(modl2, type = "HC1"))
-
-cov1        <- vcovHC(modl2, type = "HC1")
-robust.se1   <- sqrt(diag(cov1))
-
-stargazer(modl, type = "text",
-          se = list(NULL, robust.se1), intercept.bottom = FALSE, digits = 2, digits.extra = 2)
-
-# -------------------------------------
-# third stage model - example only
-# get real version from Michiel
-
-modl3 <- lm(yld ~ N + I(N^2) + dist2HQ + dist2market + dist2town + sex + age + years + rural +
-              education + education1555 + log(asset) + zone + surveyyear + legume +
-              hh_slope + r2,
-            data=endog)
-
-# bootstrap se
+library(boot)
+library(AER)
 library(stargazer)
-coeftest(modl3, df = Inf, vcov = vcovHC(modl3, type = "HC1"))
 
-cov1        <- vcovHC(modl3, type = "HC1")
-robust.se1   <- sqrt(diag(cov1))
+refit <- function(data, i){
+  coef(tobit(N ~ dist2HQ + dist2market + dist2town + sex + age + years + rural +
+                                        education + education1555 + N1555 + log(asset) + zone + surveyyear +
+                                        legume + hh_slope + r1
+                                      , data=data[i,]))
+}
 
-stargazer(modl3, type = "text",
-          se = list(NULL, robust.se1), intercept.bottom = FALSE, digits = 2, digits.extra = 2)
+
+boot.out <- boot(endog, refit, R = 500) # I have seen 500 eg in liverpool-tasie 
+
+# grab the standard errors and pass to stargazer
+sds <- apply(boot.out$t, 2, sd)
+names(sds) <- names(coef(modl.tobit))
+
+# put p.auto to TRUE to calcualte the
+# p-vals from supplued se
+stargazer(modl.tobit, type = "text",
+          se = list(sds), p.auto=TRUE,
+          intercept.bottom = FALSE,
+          digits = 2, digits.extra = 2)
+
+# get residuals for the third stage
+r2 <- residuals(modl.tobit, type="deviance")
+
