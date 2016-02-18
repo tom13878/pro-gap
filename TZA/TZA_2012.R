a861f052ff35499a3835a846c65612c9f1a18306
tom13878 <tomas.morley@wur.nl>
2016-02-18 13:03:49 +0100

    Added new TZA file with small changes to landholdings

--- TZA/TZA_2012.R
+++ TZA/TZA_2012.R
@@ -3,11 +3,12 @@
 #######################################
 
 # WDswitch
-# dataPath <- "D:\\Data\\IPOP\\SurveyData\\"
-# wdPath <- "D:\\Dropbox\\Michiel_research\\2285000066 Africa Maize Yield Gap"
-# setwd(wdPath)
+dataPath <- "D:\\Data\\IPOP\\SurveyData\\"
+extraDataPath <- "D:\\Dropbox\\Michiel_research\\2285000066 Africa Maize Yield Gap\\Analysis\\TZA\\Data"
+wdPath <- "D:\\Dropbox\\Michiel_research\\2285000066 Africa Maize Yield Gap"
+setwd(wdPath)
 
-dataPath <- "C:/Users/Tomas/Documents/LEI/data/TZA/TZA_2012_LSMS_v01_M_STATA_English_labels"
+#dataPath <- "C:/Users/Tomas/Documents/LEI/data/TZA/TZA_2012_LSMS_v01_M_STATA_English_labels"
 
 
 library(haven)
@@ -20,8 +21,8 @@ options(scipen=999)
 #######################################
 
 # WDswitch
-# oput <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_4A.dta")) %>%
-oput <- read_dta(file.path(dataPath, "AG_SEC_4A.dta")) %>%
+oput <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_4A.dta")) %>%
+# oput <- read_dta(file.path(dataPath, "AG_SEC_4A.dta")) %>%
   dplyr::select(y3_hhid, plotnum, zaocode, inter_crop=ag4a_04,
          harv_area=ag4a_21, qty=ag4a_28, valu=ag4a_29, hybrd=ag4a_08)
 
@@ -48,10 +49,10 @@ rm(list=c("oput", "legumes"))
 #######################################
 
 # WDswitch
-# plot <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_3A.dta"))%>%
-plot <- read_dta(file.path(dataPath, "AG_SEC_3A.dta")) %>%
+plot <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_3A.dta"))%>%
+# plot <- read_dta(file.path(dataPath, "AG_SEC_3A.dta"))%>%
   dplyr::select(y3_hhid, plotnum, maize=ag3a_07_2,  soil=ag3a_10, slope_farmer=ag3a_17, irrig=ag3a_18, title=ag3a_28, 
-         manure=ag3a_41, pest=ag3a_60, pest_q=ag3a_62_1, pest_q_unit=ag3a_62_2, fallow_year=ag3a_22, fallow=ag3a_23)
+                manure=ag3a_41, pest=ag3a_60, pest_q=ag3a_62_1, pest_q_unit=ag3a_62_2, fallow_year=ag3a_22, fallow=ag3a_23)
 
 plot$maize <- ifelse(plot$maize %in% 11, 1, 0)
 plot$soil <- factor(plot$soil, levels=c(1,2,3,4), labels=c("Sandy", "Loam", "Clay", "Other"))
@@ -75,13 +76,13 @@ plot$fallow <- ifelse(is.na(plot$fallow_year), NA, plot$fallow)
 plot <- dplyr::select(plot, -fallow_year, -pest_q_unit)
 
 # WDswitch
-# fert1 <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_3A.dta")) %>%
-fert1 <- read_dta(file.path(dataPath, "AG_SEC_3A.dta")) %>%
+fert1 <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_3A.dta")) %>%
+#fert1 <- read_dta(file.path(dataPath, "AG_SEC_3A.dta")) %>%
   dplyr::select(y3_hhid, plotnum, typ=ag3a_48, qty=ag3a_49, vouch=ag3a_50, valu=ag3a_51)
 
 # WDswitch
-# fert2 <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_3A.dta")) %>%
-fert2 <- read_dta(file.path(dataPath, "AG_SEC_3A.dta")) %>%
+fert2 <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_3A.dta")) %>%
+#fert2 <- read_dta(file.path(dataPath, "AG_SEC_3A.dta")) %>%
   dplyr::select(y3_hhid, plotnum, typ=ag3a_55, qty=ag3a_56, vouch=ag3a_57, valu=ag3a_58)
 
 fert1$typ <- as_factor(fert1$typ)
@@ -100,7 +101,7 @@ levels(fert1$typ) <- levels(fert2$typ) <-
 # Data on NPK composition from Sheahan et al (2014), Food Policy
 # -------------------------------------
 
-conv <- read.csv(paste(dataPath, "..", "Fert_comp.csv", sep="/")) %>%
+conv <- read.csv(file.path(extraDataPath, "Fert_comp.csv")) %>%
   transmute(typ=Fert_type2, n=N_share/100, p=P_share/100) %>%
   filter(typ %in% levels(fert1$typ))
 
@@ -157,8 +158,8 @@ rm(list=c("fert1", "fert2", "fert", "fertsub", "fertnosub", "fertmix"))
 #######################################
 
 # WDswitch
-# lab <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_3A.dta")) %>%
-lab <- read_dta(file.path(dataPath, "AG_SEC_3A.dta")) %>%
+lab <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_3A.dta")) %>%
+#lab <- read_dta(file.path(dataPath, "AG_SEC_3A.dta")) %>%
   dplyr::select(y3_hhid, plotnum, ag3a_72_id1:ag3a_74_16 )
 
 # calculate the total labour days for hired and family labour.
@@ -182,8 +183,8 @@ lab$lab <- ifelse(lab$lab %in% 0, NA, lab$lab)
 #######################################
 
 # WDswitch
-# implmt <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_11.dta")) %>%
-implmt <- read_dta(file.path(dataPath, "AG_SEC_11.dta")) %>%
+implmt <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_11.dta")) %>%
+#implmt <- read_dta(file.path(dataPath, "AG_SEC_11.dta")) %>%
   dplyr::select(y3_hhid, itemname, qty=ag11_01, valu=ag11_02) %>%
   filter(!qty %in% 0, !is.na(qty), !valu %in% 0, !is.na(valu)) %>%
   transmute(y3_hhid, valu=qty*valu) %>%
@@ -191,57 +192,60 @@ implmt <- read_dta(file.path(dataPath, "AG_SEC_11.dta")) %>%
       summarise(value=sum(valu))
 
 #######################################
-############ rural/urban ##############
-#######################################
-
-# WDswitch
-# rural <-  read_dta(file.path(dataPath, "TZA\\2012\\Data\\HH_SEC_A.dta")) %>%
-rural <-  read_dta(file.path(dataPath, "HH_SEC_A.dta")) %>%
-    dplyr::select(y3_hhid, rural=y3_rural)
-
-rural$rural <- ifelse(rural$rural %in% 1, 1, 0)
-
-#######################################
 ############### GEO ###################
 #######################################
 
+loc <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\HH_SEC_A.dta")) %>%
+  dplyr::select(ea_id2012=hh_a04_1, y3_hhid, rural=y3_rural, region_code_lsms=hh_a01_1, district_code_lsms=hh_a02_1) %>%
+  mutate(rural = ifelse(rural == 1, 1, 0)) 
+
+# We use information from TZA_2012 to add labels in a systematic way in all years.
+# Only labels for regions are added as districts are not used in the analysis.
+loc$region_name_lsms <- factor(loc$region_code_lsms, 
+                               levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 51, 52, 53, 54, 55),
+                               labels = c("Dodoma", "Arusha", "Kilimanjaro", "Tanga", "Morogoro", "Pwani", 
+                                          "Dar es Salaam", "Lindi", "Mtwara", "Ruvuma", "Iringa", "Mbeya",
+                                          "Singida", "Tabora", "Rukwa", "Kigoma", "Shinyanga", "Kagera", "Mwanza", 
+                                          "Mara", "Manyara", "Kaskazini Unguja", "Kusini Unguja", "Mjini Magharibi",
+                                          "Kaskazini Pemba", "Kusini Pemba"))
+
+
+# add a zone variable
+loc$zone[loc$region_name_lsms %in% c("Kagera","Mwanza", "Mara")] <- "Lake"
+loc$zone[loc$region_name_lsms %in% c("Shinyanga","Kigoma", "Tabora")] <- "Western"
+loc$zone[loc$region_name_lsms %in% c("Arusha","Kilimanjaro", "Manyara", "Tanga")] <- "Northern"
+loc$zone[loc$region_name_lsms %in% c("Singida","Dodoma")] <- "Central"
+loc$zone[loc$region_name_lsms %in% c("Rukwa", "Mbeya","Iringa")] <- "Southern Highlands"
+loc$zone[loc$region_name_lsms %in% c("Pwani","Morogoro", "Dar-Es-Salaam")] <- "Eastern"
+loc$zone[loc$region_name_lsms %in% c("Lindi","Ruvuma", "Mtwara")] <- "Southern"
+loc$zone[loc$region_name_lsms %in% c("Dar es Salaam")] <- "Dar es Salaam" 
+loc$zone[loc$region_name_lsms %in% c("Kaskazini Unguja", "Kaskazini Unguja", "Kusini Pemba", "Kusini Unguja",
+                                     "Kaskazini Pemba", "Mjini Magharibi")] <- "Zanzibar"
+
+loc$zone <- factor(loc$zone)
+
 # WDswitch
-# geo <- read.csv(file.path(wdPath, "Analysis\\TZA\\Data\\TZA_geo_total_2012.csv"), stringsAsFactors=F) %>%
-geo <- readRDS("C:/Users/Tomas/Documents/LEI/Data/TZA/TZA_data_2012.rds") %>% 
-  dplyr::select(y3_hhid, lon, lat, plotnum, SPEI, RootDepth, region=NAME_1,
+geo <- readRDS(file.path(wdPath, "Analysis\\TZA\\Data\\TZA_geo_2012.rds")) %>%
+  dplyr::select(y3_hhid, lon, lat, plotnum, SPEI, RootDepth, region_name=NAME_1, district_name=NAME_2,
                 AEZ=land03, ph=ph_sd1_sd3, ph2=ph_sd1_sd5,
-                SOC=SOC_sd1_sd3, SOC2=SOC_sd1_sd5, rain=gsRainfall,
-                plot01,
-                dist01, dist02, dist03, dist04, dist05,
-                clim02,clim04, clim05, 
-                soil01, soil02, soil03, soil04, soil05, soil06, soil07, soil08, soil09, soil10, soil11,
-                crops04, crops05, crops07, crops08,
+                SOC=SOC_sd1_sd3, SOC2=SOC_sd1_sd5, rain_CRU=gsRainfall,
+                dist_hh=plot01, 
+                #plot_slope=plot02, plot_elevation=plot03, plot_pwi=plot04, # variables not present in 2010 data
+                dist_road=dist01, dist_popcenter=dist02, dist_market=dist03, dist_borderpost=dist04, dist_regcap=dist05,
+                hh_elevation=soil01, hh_slope=soil02, hh_twi=soil03, 
+                rain_year=crops07, rain_wq=crops08,
                 YA, YW, YP) %>%
   unique()
 
-geo$AEZ <- as.factor(geo$AEZ)
-geo$y3_hhid <- as.character(geo$y3_hhid)
 
-# add a zone variable
-geo$zone[geo$region %in% c("Kagera","Mwanza", "Mara")] <- "Lake"
-geo$zone[geo$region %in% c("Shinyanga","Kigoma", "Tabora")] <- "Western"
-geo$zone[geo$region %in% c("Arusha","Kilimanjaro", "Manyara", "Tanga")] <- "Northern"
-geo$zone[geo$region %in% c("Singida","Dodoma")] <- "Central"
-geo$zone[geo$region %in% c("Rukwa", "Mbeya","Iringa")] <- "Southern Highlands"
-geo$zone[geo$region %in% c("Pwani","Morogoro", "Dar-Es-Salaam")] <- "Eastern"
-geo$zone[geo$region %in% c("Lindi","Ruvuma", "Mtwara")] <- "Southern"
-geo$zone[geo$region %in% c("Kaskazini-Unguja", "Zanzibar South and Central", "Kusini-Pemba",
-                           "Kaskazini-Pemba", "Zanzibar West")] <- "Zanzibar"
-
-geo$zone <- factor(geo$zone)
 
 #######################################
 ############### AREAs #################
 #######################################
 
 # WDswitch
-# areas <- read.csv("Analysis/TZA/Data/areas_w3.csv") %>%
-areas <- read.csv("C:/Users/Tomas/Documents/LEI/data/TZA/areas_w3.csv") %>%
+areas <- read.csv("Analysis/TZA/Data/areas_w3.csv") %>%
+# areas <- read.csv("C:/Users/Tomas/Documents/LEI/data/TZA/areas_w3.csv") %>%
   dplyr::select(y3_hhid, plotnum,
                 area_farmer=area.est,
                 area_gps=gps_imputed)
@@ -254,17 +258,16 @@ areas$area_gps <- ifelse(areas$area_gps %in% 0, NA, areas$area_gps)
 areas$area_gps <- areas$area_gps*0.404686 # from wikipedia
 
 areaTotal <- group_by(areas, y3_hhid) %>%
-  summarise(area_tot = sum(area_gps, na.rm=TRUE))
+  summarise(area_tot = sum(area_gps))
 
-areaTotal$area_tot <- ifelse(areaTotal$area_tot %in% 0, NA, areaTotal$area_tot)
 
 #######################################
 ########## TRANSPORT COSTS ############
 #######################################
 
 # WDswitch
-# tc <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_5A.dta")) %>%
-tc <- read_dta(file.path(dataPath, "AG_SEC_5A.dta")) %>%
+tc <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_5A.dta")) %>%
+# tc <- read_dta(file.path(dataPath, "AG_SEC_5A.dta")) %>%
   dplyr::filter(zaocode %in% 11) %>%
   dplyr::select(y3_hhid, trans=ag5a_18, trans_dist=ag5a_19, trans_cost=ag5a_22)
 
@@ -275,58 +278,41 @@ tc$trans <- ifelse(tc$trans %in% 1, 1, 0)
 #######################################
 
 # WDswitch
-# HH12 <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\HH_SEC_B.dta")) %>%
-HH12 <- read_dta(file.path(dataPath, "HH_SEC_B.dta")) %>%
-  select(y3_hhid, indidy3, status=hh_b05, sex=hh_b02,
-         yob=hh_b03_1, age=hh_b04, years=hh_b26)
+se <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\HH_SEC_B.dta")) %>%
+# se <- read_dta(file.path(dataPath, "HH_SEC_B.dta")) %>%
+  filter(hh_b05 %in% 1) %>% # 1 for head of household
+  dplyr::select(y3_hhid, indidy3, sex=hh_b02, yob=hh_b03_1, age=hh_b04)
 
-# 99 in years variable means that the household member has
-# lived in the community all his/her life
-HH12$years <- as.numeric(HH12$years)
-HH12$years <- ifelse(HH12$years %in% 99, HH12$age, HH12$years)
-HH12$status <- as_factor(HH12$status)
-HH12$sex <- as_factor(HH12$sex)
-HH12$yob <- as.integer(HH12$yob)
+se$sex <- ifelse(se$sex %in% 2, 1, 0)
+se$yob <- as.integer(as.character(se$yob))
 
-HH12$cage <- cut(HH12$age, breaks = c(0, 15, 55, max(HH12$age, na.rm=TRUE)),
-                 labels=1:3, include.lowest = TRUE, right = TRUE)
+# education
 
-# -------------------------------------
-# education of household head and sum of
-# education of all household members
-# between the ages of 15 and 55
-
-ed <- read_dta(file.path(dataPath, "HH_SEC_C.dta")) %>%
-  select(y3_hhid, indidy3, ed_any=hh_c03, start=hh_c04, end=hh_c08)
+# WDswitch
+ed <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\HH_SEC_C.dta")) %>%
+# ed <- read_dta(file.path(dataPath, "HH_SEC_C.dta")) %>%
+  dplyr::select(y3_hhid, indidy3, start=hh_c04, end=hh_c08)
 
 ed$end <- as.integer(as.character(ed$end))
 ed$end <- ifelse(ed$end %in% 9999, NA, ed$end)
-ed$ed_any <- as_factor(ed$ed_any) # any education
 
-# join HH10 and ed to find amount of schooling
-HH12 <- left_join(HH12, ed)
-HH12$education <- HH12$end - (HH12$yob + HH12$start)
-HH12$education <- ifelse(HH12$ed_any %in% "NO", 0, HH12$education)
-HH12 <- select(HH12, -start, -end, -yob)
+# join se and ed to find years in school
+se <- left_join(se, ed) %>% dplyr::select(-indidy3)
+rm("ed")
+
+se$educ <- se$end - (se$yob + se$start)
+se <- dplyr::select(se, -start, -end, -yob)
 
 # if anyone received negative years of schooling, set to NA
-HH12$education <- ifelse(HH12$education < 0, NA, HH12$education)
+se$educ <- ifelse(se$educ < 0, NA, se$educ)
 
-# summarise the data: get sum of education
-# of household members 15-55 and number of
-# household members 15-55
-HH12_x <- group_by(HH12, y3_hhid) %>%
-  summarise(education1555=sum(education[cage %in% 2], na.rm=T),
-            N1555=sum(cage %in% 2))
-HH12 <- left_join(HH12, HH12_x); rm(HH12_x)
-HH12 <- filter(HH12, status %in% "HEAD") %>% select(-indidy3, -status, -cage, -ed_any)
+# still some people have received a lot of schooling!!!!
 
-# -------------------------------------
 # plot ownership
 
 # WDswitch
-# own <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_3A.dta")) %>%
-own <- read_dta(file.path(dataPath, "AG_SEC_3A.dta")) %>%
+own <- read_dta(file.path(dataPath, "TZA\\2012\\Data\\AG_SEC_3A.dta")) %>%
+# own <- read_dta(file.path(dataPath, "AG_SEC_3A.dta")) %>%
   dplyr::select(y3_hhid, plotnum, own=ag3a_25)
 
 own$own <- ifelse(own$own %in% 1 | own$own %in% 5, 1, 0)
@@ -340,16 +326,15 @@ TZA2012 <- left_join(oput_maize, plot)
 TZA2012 <- left_join(TZA2012, lab)
 TZA2012 <- left_join(TZA2012, areas)
 TZA2012 <- left_join(TZA2012, own)
+TZA2012 <- left_join(TZA2012, geo)
 
 # joins at the household level
 TZA2012 <- left_join(TZA2012, implmt)
-TZA2012 <- left_join(TZA2012, geo)
-TZA2012 <- left_join(TZA2012, rural)
-TZA2012 <- left_join(TZA2012, HH12)
+TZA2012 <- left_join(TZA2012, loc)
+TZA2012 <- left_join(TZA2012, se)
 TZA2012 <- left_join(TZA2012, tc)
 TZA2012 <- left_join(TZA2012, areaTotal)
 
-rm(list=ls()[!ls() %in% "TZA2012"])
 
 # -------------------------------------
 # Make some new variables
@@ -357,26 +342,22 @@ rm(list=ls()[!ls() %in% "TZA2012"])
 
 # per hectacre
 TZA2012 <- mutate(TZA2012,
-              yld=qty/area_gps,
-              N=N/area_gps,
-              P=P/area_gps,
-              lab=lab/area_gps,
-              pest_q=pest_q/area_gps,
-              asset=value/area_tot
+                  yld=qty/area_gps,
+                  N=N/area_gps,
+                  P=P/area_gps,
+                  lab=lab/area_gps,
+                  pest_q=pest_q/area_gps,
+                  asset=value/area_tot
 )
 
 TZA2012 <- dplyr::select(TZA2012, -qty, -value)
 
-# add final variables
-TZA2012 <- mutate(TZA2012,
-              N2=N^2,
-              asset2=asset^2,
-              area2=area_gps^2,
-              lab2=lab^2,
-              surveyyear=2012
-)
+# add and rename final variables
+TZA2012 <- mutate(TZA2012, surveyyear=2012) %>% rename(hhid2012=y3_hhid)
+              
+
 
 # save to file
-# save(TZA2012, file=".\\Analysis\\TZA\\Data\\TZA12_data.RData")
-# write.csv(TZA2012, "C:/Users/Tomas/Documents/LEI/TZA12_data.csv", row.names=FALSE)
+saveRDS(TZA2012, file=".\\Analysis\\TZA\\Data\\TZA_data_2012.rds")
+#write.csv(TZA2012, "C:/Users/Tomas/Documents/LEI/TZA12_data.csv", row.names=FALSE)
 
